89c487b9bdea054b5d1542f17bf75ac300d70ac9
Raissia Freitas <raiissiasylva@gmail.com>
2023-09-26 21:55:50 -0300

    26/09

---
+++ src/modules/catalago/domain/categoria/categoria.repository.interface.ts
@@ -0,0 +1,8 @@
+import { IRepository } from "@shared/domain/repository.interface";
+import { Interface } from "readline";
+
+interface ICategoriaRepository<T> extends IRepository<T> {
+
+}
+
+export { ICategoriaRepository}
