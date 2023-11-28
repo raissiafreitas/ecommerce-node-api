6dd5cd7168988e0da78a663f13312b9e6ce8759b
Raissia Freitas <raiissiasylva@gmail.com>
2023-11-27 21:49:37 -0300

    implementação de api dia 27/11/2023

---
+++ src/main/index.ts
@@ -0,0 +1,27 @@
+
+import { createHTTPServer } from "./presentation/http/server";
+import dotenv from 'dotenv';
+
+async function bootstrap() {
+
+    //Carrega variáveis de ambiente do arquivo .env
+    dotenv.config();
+
+    const api_name = process.env.API_NAME;
+    const host_name = process.env.HOST_NAME;
+    const port = process.env.PORT;
+
+    console.log(`[${api_name}] 🚀 Inicializando a API....`);
+
+    const httpServer = await createHTTPServer();
+
+    httpServer.listen({ port: port }, async () => {
+        console.log(`[${api_name}] ✅ Servidor HTTP pronto e ouvindo em http://${host_name}:${port}`);
+    });
+
+}
+
+bootstrap()
+    .catch((error) => {
+        console.error(error);
+    });
