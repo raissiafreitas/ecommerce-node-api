7d553332dfb74ec041ee38950738bf897155b75e
Raissia Freitas <raiissiasylva@gmail.com>
2023-12-04 21:05:40 -0300

    parte 30 atualizar e deletar 04/12/23

--- src/modules/catalogo/presentation/http/rest/categoria.routes.ts
+++ src/modules/catalogo/presentation/http/rest/categoria.routes.ts
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import {  inserirCategoriaController, recuperarCategoriaPorIdController, recuperarTodasCategoriasController } from './controllers';
+import {  atualizarCategoriaController, deletarCategoriaController, inserirCategoriaController, recuperarCategoriaPorIdController, recuperarTodasCategoriasController } from './controllers';
 
 const categoriaRouter = express.Router();
 
@@ -18,5 +18,16 @@ categoriaRouter.post(
     '/',
     (request, response, next) =>  inserirCategoriaController.inserir(request, response, next)
 );
+
+categoriaRouter.put(
+    '/:id',
+    (request, response, next) =>  atualizarCategoriaController.atualizar(request, response, next)
+)
+
+categoriaRouter.delete(
+    '/:id',
+    (request, response, next) =>  deletarCategoriaController.deletar(request, response, next)
+)
+
 
 export { categoriaRouter };
--- src/modules/catalogo/presentation/http/rest/controllers/index.ts
+++ src/modules/catalogo/presentation/http/rest/controllers/index.ts
@@ -1,14 +1,20 @@
-import { inserirCategoriaUseCase, recuperarCategoriaPorIdUseCase, recuperarTodasCategoriasUseCase } from "@modules/catalogo/application/use-case";
+import { atualizarCategoriaUseCase, deletarCategoriaUseCase, inserirCategoriaUseCase, recuperarCategoriaPorIdUseCase, recuperarTodasCategoriasUseCase } from "@modules/catalogo/application/use-case";
 import { RecuperarCategoriaPorIdExpressController } from "./recuperar-categoria-por-id.express.controllers";
 import { RecuperarTodasCategoriaExpressController } from "./recuperar-todas-categorias.express.controllers"; 
 import { InserirCategoriaExpressController } from "./inserir-categoria.express.controllers"; 
+import { AtualizarCategoriaExpressController } from "./atualizar-categoria.express.controller";
+import { DeletarCategoriaExpressController } from "./deletar-categoria.express.controller";
 
 const recuperarCategoriaPorIdController = new RecuperarCategoriaPorIdExpressController(recuperarCategoriaPorIdUseCase);
 const recuperarTodasCategoriasController = new RecuperarTodasCategoriaExpressController(recuperarTodasCategoriasUseCase);
 const inserirCategoriaController = new InserirCategoriaExpressController(inserirCategoriaUseCase);
+const atualizarCategoriaController = new AtualizarCategoriaExpressController(atualizarCategoriaUseCase);
+const deletarCategoriaController = new DeletarCategoriaExpressController(deletarCategoriaUseCase);
 
 export {
     recuperarCategoriaPorIdController,
     recuperarTodasCategoriasController,
-    inserirCategoriaController
+    inserirCategoriaController,
+    atualizarCategoriaController,
+    deletarCategoriaController
 }
